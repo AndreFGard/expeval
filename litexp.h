417e357fbc493da6f98ea8c422f9428dc5eaac61
AndreFGard <96950645+AndreFGard@users.noreply.github.com>
2025-01-10 11:30:52 -0300

    make clear that other methods from litexp must be defined by deriveds

--- litexp.h
+++ litexp.h
@@ -1,5 +1,7 @@
 using namespace std;
 #include <string>
+//todo: static assert to force the implementation of methods
+//this is crtp like, but also creates vtables as runtime polymorphism makes the code smaller
 template <typename Derive, typename val_type>
 class LitExp
 {
@@ -8,12 +10,12 @@ class LitExp
         val_type val;
         //     const inline  virtual string toStr() const = 0;
     public:
-        virtual inline val_type getVal() const;
-        virtual const val_type &add(LitExp *b);
+        virtual inline val_type getVal() const = 0;
+        virtual const val_type &add(LitExp *b) = 0;
 
         // const inline string getExpStr() const { return expStr; }
         // friend ostream &operator<<(ostream &os, const Expr &expr);
     protected:
-        virtual Derive* is_compatible(LitExp *b);
+        virtual Derive* is_compatible(LitExp *b) = 0;
         LitExp() = default; //prohibiting creation of Litexps, as shown in the documentation
 };
