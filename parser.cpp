b8cb348ab52d98ec37c93ab6cf61f69446a3f112
Andrefgard <96950645+AndreFGard@users.noreply.github.com>
2025-01-12 11:12:02 -0300

    add operator size to operator class

--- parser.cpp
+++ parser.cpp
@@ -17,25 +17,40 @@ enum class OperatorType {
 class Operator {
     private:
         OperatorType type;
+        int size;
     public:
         Operator(string_view op){
             if (op == "+"){
                 type = OperatorType::Add;
+                size = 1;
             } else if (op == "-"){
                 type = OperatorType::Subtract;
+                size =1;
             } else if (op == "*"){
                 type = OperatorType::Multiply;
+                size =1;
             } else if (op == "/"){
                 type = OperatorType::Divide;
+                size =1;
             } else if (op == "&&"){
                 type = OperatorType::And;
+                size =2;
             } else if (op == "||"){
                 type = OperatorType::Or;
+                size = 2;
             } else if (op == "!"){
+                size = 1;
                 type = OperatorType::Not;
             } else {
                 throw invalid_argument("Invalid operator");
             }
+        }
+       inline OperatorType getType(){
+            return type;
+        }
+        //returns the size of the string of the operator
+        inline int getSize(){
+            return size;
         }
 };
 
@@ -57,9 +72,13 @@ class ExpressionString: public string{
        //todo use operator as an enum or sum 
         Operator parseOperator(){
             return Operator(view.substr(0, 2));
+            moveView(2);
         }
-        
+
     private:
+        inline void moveView(int n){
+            view = view.substr(2);
+        }
         string_view view;
         int index =0;
 };
