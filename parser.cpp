a211763e141cbae35fe05ea87d952b5138eec381
Andrefgard <96950645+AndreFGard@users.noreply.github.com>
2025-01-11 22:12:08 -0300

    start implementing parser.cpp

--- parser.cpp
+++ parser.cpp
@@ -4,13 +4,68 @@
 #include <variant>
 using namespace std;
 
+class ExpressionString: public string{
+    public:
+        ExpressionString(string_view str): string(str){
+            view = string_view(str);
+        }
+        string getOriginalString(){
+            return *this;
+        }
+        
+        bool startsWith(string prefix){
+            return (view.rfind(prefix,0) == 0);
+        }
+       //todo use operator as an enum or sum 
+        Operator parseOperator(){
+            if ()
+        }
+    private:
+        string_view view;
+        int index =0;
+
+class Operator{
+    public:
+    //todo add errors
+        Operator(string_view op): op(op){
+            if (op)
+        }
+    private:
+        string_view op;
+}
+
+
 class Parser{
     public:
-        Parser();
+        Parser()
+        {
+
+        }
+
+
     private:
-        variant<ArithExp, LogicExp> parse_val;
+        int currentToken = 0;
+        string_view curview;
+
+        bool starts_with(string prefix){
+            return( curview.rfind(prefix,0) == 0);
+        }
+
         void parse(string expStr){
+            variant<ArithExp, LogicExp> parse_val(parse_or_exp(expStr));            
+        }
+
+        variant<ArithExp, LogicExp> parse_or_exp(string expStr){
+            variant<ArithExp, LogicExp> 
+                val(parse_and_exp(expStr));
+
+            if (starts_with(string("||"))){
+                
+            }
+
+            return val;
             
+
         }
 
 };
